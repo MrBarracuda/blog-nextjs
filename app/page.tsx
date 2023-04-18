28f8bdb372bc4e2bd7794c795b25f2f359d564b9
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2023-04-18 17:20:38 +0300

    Implement data fetch from json server

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
-// import Image from 'next/image'
-// import { Inter } from 'next/font/google'
-//
-// const inter = Inter({ subsets: ['latin'] })
+type Item = {
+  id: string;
+  title: string;
+  summary: string;
+  content: string;
+  author: string;
+  publicationDate: string;
+}
 
-export default function Home() {
+type Items = Item[];
+
+export default async function Home() {
+  const data = await fetch('https://blog-app-next-js-json-server-git-main-thebarracuda.vercel.app/articles');
+  const res: Items = await data.json();
+
   return (
     <main>
       <h1 className="text-2xl py-2 m-4 uppercase">Blog app nextjs</h1>
+      <ul>
+        {res.map((item) => (
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.summary}</p>
+            <p>{item.content}</p>
+            <p>{item.author}</p>
+            <p>{item.publicationDate}</p>
+          </div>
+        ))}
+      </ul>
     </main>
   )
 }
